6971c21defda99dee97d950cca65ecf0d864ec0d
undancer <undancer+github@gmail.com>
2023-09-03 14:25:31 +0800

    修改配置

--- packages/app/astro.config.ts
+++ packages/app/astro.config.ts
@@ -1,11 +1,17 @@
 import netlify from '@astrojs/netlify/functions'
 import { defineConfig } from 'astro/config'
+import UnoCSS from 'unocss/astro'
 
 // https://astro.build/config
 
 export default /** @type {import('astro').AstroUserConfig} */ defineConfig({
   // 你的配置项都在这里
   // https://docs.astro.build/zh-cn/reference/configuration-reference/
+  integrations: [
+    UnoCSS({
+      injectReset: true,
+    }),
+  ],
   output: 'server',
   adapter: netlify({
     dist: new URL('./dist/', import.meta.url),
