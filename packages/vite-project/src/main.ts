b6a0be15f4f19fc588139e88e3dff6e6a5a77eb4
undancer <undancer+github@gmail.com>
2023-09-03 18:02:02 +0800

    暂存代码

--- packages/vite-project/src/main.ts
+++ packages/vite-project/src/main.ts
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+// import { createApp } from 'vue'
+// import './style.css'
+// import App from './App.vue'
+//
+// createApp(App).mount('#app')
 
-createApp(App).mount('#app')
+import '@unocss/reset/normalize.css'
+import '@unocss/reset/sanitize/sanitize.css'
+import '@unocss/reset/sanitize/assets.css'
+import '@unocss/reset/tailwind.css'
+
+// import 'uno.css'
+import 'virtual:uno.css'
--- packages/vite-project/vite.config.ts
+++ packages/vite-project/vite.config.ts
@@ -1,7 +1,14 @@
+import { presetDaisy } from 'unocss-preset-daisy'
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import unocss from 'unocss/vite'
+import { presetIcons, presetUno } from 'unocss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    // vue(),
+    unocss({
+      presets: [presetUno(), presetDaisy(), presetIcons()],
+    }),
+  ],
 })
