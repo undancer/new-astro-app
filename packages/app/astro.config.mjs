72155e54164b7b1a4571e6be86b8fbc9c49ea626
undancer <undancer+github@gmail.com>
2023-09-03 11:53:58 +0800

    修改配置

--- packages/app/astro.config.mjs
+++ packages/app/astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config'
-
 import netlify from '@astrojs/netlify/functions'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: netlify(),
+  adapter: netlify({
+    dist: new URL('./dist/', import.meta.url),
+  }),
 })
