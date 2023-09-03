cfb8d87d736ec8a3205dc9c370a79d8f52e369e7
undancer <undancer+github@gmail.com>
2023-09-03 13:06:24 +0800

    修改配置

--- packages/app/astro.config.ts
+++ packages/app/astro.config.ts
@@ -2,7 +2,10 @@ import netlify from '@astrojs/netlify/functions'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({
+
+export default /** @type {import('astro').AstroUserConfig} */ defineConfig({
+  // 你的配置项都在这里
+  // https://docs.astro.build/zh-cn/reference/configuration-reference/
   output: 'server',
   adapter: netlify({
     dist: new URL('./dist/', import.meta.url),
